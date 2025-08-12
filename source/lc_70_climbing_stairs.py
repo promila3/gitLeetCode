5e4575503b76e4719cb0a8e860890946bd433f85
Promila Agarwal <promila3@gmail.com>
2025-08-12 16:55:18 -0700

    BRUTE force solution added

--- source/lc_70_climbing_stairs.py
+++ source/lc_70_climbing_stairs.py
@@ -2,6 +2,14 @@ class ClimbingStairs():
     def __init__(self):
         pass
 
-    def solve(self):
-        return False
+    def climb(self, i: int, n: int) -> int:
+        if i > n:
+            return 0
+        if i == n:
+            return 1
+        return self.climb(i+1, n) + self.climb(i+2, n)
+    
+    def solve(self, n: int)->int:
+        return self.climb(0, n)
+        
     
--- tests/test_lc_70_climbing_stairs.py
+++ tests/test_lc_70_climbing_stairs.py
@@ -3,5 +3,7 @@ import setup_path
 from lc_70_climbing_stairs  import ClimbingStairs
 
 def test_solve():
+    input = 2
+    output = 2
     solution = ClimbingStairs()
-    assert solution.solve()
+    assert solution.solve(input) == output
